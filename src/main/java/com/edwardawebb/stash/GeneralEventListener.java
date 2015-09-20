d44aa108fb13e73f1b52a36c322c3c2c27ad9f22
Eddie Webb <ollitech@gmail.com>
2015-09-20 15:35:22 -0400

    Sweet, project and repo permission events let me block before key is created.

--- src/main/java/com/edwardawebb/stash/GeneralEventListener.java
+++ src/main/java/com/edwardawebb/stash/GeneralEventListener.java
@@ -7,48 +7,65 @@ import org.apache.log4j.Logger;
 
 import com.atlassian.event.api.EventListener;
 import com.atlassian.stash.event.StashEvent;
+import com.atlassian.stash.event.permission.ProjectPermissionGrantRequestedEvent;
+import com.atlassian.stash.event.permission.RepositoryPermissionGrantRequestedEvent;
+import com.atlassian.stash.i18n.I18nService;
 import com.atlassian.stash.ssh.api.SshKey;
 import com.atlassian.stash.ssh.api.SshKeyService;
-import com.atlassian.stash.user.StashUser;
+import com.atlassian.stash.user.UserType;
 import com.edwardawebb.stash.ssh.EnterpriseSshKeyService;
 
 public class GeneralEventListener {
     private final Logger logger = Logger.getLogger(GeneralEventListener.class);
-    
-    private final static String SSH_KEY_CREATED_EVENT_CLASS="com.atlassian.stash.ssh.SshKeyCreatedEvent";
-    
-   
+
+    private final static String SSH_KEY_CREATED_EVENT_CLASS = "com.atlassian.stash.ssh.SshKeyCreatedEvent";
+    private final static String INTERNAL_SERVICE_USER_CLASS_NAME = "com.atlassian.stash.internal.user.InternalServiceUser";
+
     final private EnterpriseSshKeyService enterpriseSshKeyService;
-    
-    public GeneralEventListener(SshKeyService sshKeyService,EnterpriseSshKeyService enterpriseSshKeyService) {
-         this.enterpriseSshKeyService = enterpriseSshKeyService;
+
+    private I18nService i18nService;
+
+    public GeneralEventListener(SshKeyService sshKeyService, EnterpriseSshKeyService enterpriseSshKeyService,I18nService i18nService) {
+        this.enterpriseSshKeyService = enterpriseSshKeyService;
+        this.i18nService = i18nService;
     }
 
     @EventListener
-    public void mylistener(StashEvent stashEvent)
-    {
-        
+    public void mylistener(StashEvent stashEvent) {
+
         // READ THIS, and I'm sorry.
-        // I know reflection is BS and brittle, but it was the only way to get at the public "SshKey" using a non-prublic event.
-        // Reflection works using our own class that mimics Atlassian object. BUT this cannot be casted to the object (ours or atlassians)
+        // I know reflection is BS and brittle, but it was the only way to get
+        // at the public "SshKey" using a non-prublic event.
+        // Reflection works using our own class that mimics Atlassian object.
+        // BUT this cannot be casted to the object (ours or atlassians)
         // https://developer.atlassian.com/static/javadoc/stash/3.11.2/ssh/reference/com/atlassian/stash/ssh/SshKeyCreatedEvent.html
         // https://maven.atlassian.com/#nexus-search;classname~com.atlassian.stash.ssh.SshKeyCreatedEvent
-        // Dependency included as provided compiles, but fails run time with no class def
-        // Dependency included as compile and it compiles, but fails at run time with some other class failing..
+        // <pre>Note: in Stash 2.9, SshKeyEvent and all of it's subclasses will
+        // never be published. The classes are not OSGI exported and can
+        // therefore not be published. Instead, the events are translated into
+        // AuditEvents, which are published.</pre>
+        // -https://developer.atlassian.com/static/javadoc/stash/3.7.1/ssh/reference/com/atlassian/stash/internal/key/ssh/SshKeyAccessRevokedEvent.html
+        // Dependency included as provided compiles, but fails run time with no
+        // class def
+        // Dependency included as compile and it compiles, but fails at run time
+        // with some other class failing..
 
-        
-        if ( SSH_KEY_CREATED_EVENT_CLASS.equals(stashEvent.getClass().getCanonicalName())){
-            if(logger.isDebugEnabled()){
-                logger.debug("SSH key was created by" + stashEvent.getUser().getDisplayName() + " from " + stashEvent.getSource());
+        logger.warn("AuditEVENT=====================" + stashEvent.getClass() + " from " + stashEvent.getSource());
+
+        if (SSH_KEY_CREATED_EVENT_CLASS.equals(stashEvent.getClass().getCanonicalName())) {
+            if (logger.isDebugEnabled()) {
+                logger.debug("SSH key was created by" + stashEvent.getUser().getDisplayName() + " from "
+                        + stashEvent.getSource());
             }
             try {
                 Method method = stashEvent.getClass().getMethod("getKey");
                 SshKey key = (SshKey) method.invoke(stashEvent);
-                
-               if(logger.isDebugEnabled()){
-                   logger.debug("The SSH Key is: " + key.getLabel() + "|with value: " + key.getText() + "|with user: " + key.getUser());
-               }
-               enterpriseSshKeyService.removeKeyIfNotLegal(key,stashEvent.getUser());
+
+                if (logger.isDebugEnabled()) {
+                    logger.debug("The SSH Key is: " + key.getLabel() + "|with value: " + key.getText() + "|with user: "
+                            + key.getUser());
+                }
+                enterpriseSshKeyService.removeKeyIfNotLegal(key, stashEvent.getUser());
             } catch (IllegalArgumentException e) {
                 // TODO Auto-generated catch block
                 e.printStackTrace();
@@ -65,14 +82,26 @@ public class GeneralEventListener {
                 // TODO Auto-generated catch block
                 e.printStackTrace();
             }
-        }
-        
+        } 
+
     }
 
-
-
-
-
-
+    @EventListener
+    public void mylistener(ProjectPermissionGrantRequestedEvent permissionRequestEvent) {
+        if (UserType.SERVICE == permissionRequestEvent.getAffectedUser().getType()) {
+            logger.warn("Rejected Permission Access attempt against project: " + permissionRequestEvent.getProject());
+            permissionRequestEvent
+                    .cancel(i18nService.createKeyedMessage("customplugin.pullrequest.cancel"));
+        }
+    }
     
+    @EventListener
+    public void mylistener(RepositoryPermissionGrantRequestedEvent permissionRequestEvent) {
+        if (UserType.SERVICE == permissionRequestEvent.getAffectedUser().getType()) {
+            logger.warn("Rejected Permission Access attempt against repository: " + permissionRequestEvent.getRepository());
+            permissionRequestEvent
+                    .cancel(i18nService.createKeyedMessage("customplugin.pullrequest.cancel"));
+        }
+    }
+
 }
