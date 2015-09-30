31f9008952c67b64179be05dc776b955ec3d99da
Eddie Webb <ollitech@gmail.com>
2015-09-30 11:51:04 -0400

    syncing up before full review

--- src/test/java/ut/com/lmig/forge/stash/ssh/keys/scheduler/KeyRotationJobRunnerTest.java
+++ src/test/java/ut/com/lmig/forge/stash/ssh/keys/scheduler/KeyRotationJobRunnerTest.java
@@ -16,10 +16,12 @@
 
 package ut.com.lmig.forge.stash.ssh.keys.scheduler;
 
-import static org.junit.Assert.fail;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyString;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import org.junit.Before;
 import org.junit.Test;
